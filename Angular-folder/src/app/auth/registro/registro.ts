f0488fe5804b9625d650ab861b0e7386e0c548ef
lesterietado <leandrocabrera7c@gmail.com>
2026-09-15 12:12:21 -0300

    avances en servicio auth

--- Angular-folder/src/app/auth/registro/registro.ts
+++ Angular-folder/src/app/auth/registro/registro.ts
@@ -1,11 +1,17 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { AuthService } from '../../servicios/auth';
 
 @Component({
   selector: 'app-registro',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule],
   templateUrl: './registro.html',
   styleUrl: './registro.css',
 })
 export class Registro {
-
+ constructor(
+  private authService: AuthService
+ ) {}
+  
 }

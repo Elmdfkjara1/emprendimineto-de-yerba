a05bac19231134d639b51b3837e2afd957e0943b
jeree1706 <qjeremias263@gmail.com>
2026-09-25 08:31:32 -0300

    Nueva sección en inicio de ¿quienes somos?

--- Angular-folder/src/app/pagina/inicio/inicio.ts
+++ Angular-folder/src/app/pagina/inicio/inicio.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
+import { AfterViewInit } from '@angular/core';
 
 @Component({
   selector: 'app-inicio',
@@ -7,4 +8,36 @@ import { RouterLink } from '@angular/router';
   templateUrl: './inicio.html',
   styleUrl: './inicio.css',
 })
-export class Inicio {}
+export class Inicio implements AfterViewInit {
+
+  ngAfterViewInit(): void {
+
+    const elementos = document.querySelectorAll(
+      '.aparecer-izquierda, .aparecer-derecha'
+    );
+
+    const observer = new IntersectionObserver(
+      (entradas) => {
+
+        entradas.forEach((entrada) => {
+
+          if (entrada.isIntersecting) {
+            entrada.target.classList.add('visible');
+            observer.unobserve(entrada.target);
+          }
+
+        });
+
+      },
+      {
+        threshold: 0.2
+      }
+    );
+
+    elementos.forEach((elemento) => {
+      observer.observe(elemento);
+    });
+
+  }
+
+}
